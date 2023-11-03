b5b1fede5cb07a97825d5cf8c01049f5d0c11d67
iancdesponds <iancdesponds@gmail.com>
2023-11-03 20:16:18 -0300

    Get e Post de Produtos

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from flask_pymongo import PyMongo
 import bcrypt
 from credentials import settings, credenciais
 
-# app = Flask(__name__)
 
-# # Configurar o MongoDB
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/seu_banco_de_dados"
-# mongo = PyMongo(app)
 app = Flask("Farmacia")
 app.secret_key = 'deena'  # Substitua por uma chave secreta forte.
 app.config["MONGO_URI"] = f"mongodb+srv://{credenciais['user_mongo']}:{credenciais['password_mongo']}@{settings['host']}/{settings['database']}?retryWrites=true&w=majority"
 mongo = PyMongo(app)
+
 
 @app.route('/')
 def home():
@@ -45,6 +42,48 @@ def login():
 def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
+
+@app.route('/produtos', methods=['GET', 'POST'])
+def produtos():
+    if request.method == 'GET':
+        produtos = list(mongo.db.produtos.find({},{'marca_produto':1, 'nome_produto':1, 'descricao_produto':1, 'quantidade_por_unidade_produto':1, 'notificacao_baixo_estoque_produto':1, '_id':0}))
+        produtos_list = [{"Marca":produto['marca_produto'], "Nome":produto['nome_produto'], "Descrição":produto['descricao_produto'], "Quantidade por Unidade":produto['quantidade_por_unidade_produto'], "Notificação de Baixo Estoque":produto['notificacao_baixo_estoque_produto']} for produto in produtos]
+        return jsonify({"Produtos":produtos_list}), 200
+    
+    elif request.method == 'POST':
+        try:
+            request_data = request.form
+
+            if 'marca_produto' not in request_data:
+                return {'ERRO': 'marca do produto não informada'}, 400
+            marca_produto = request_data.get('marca_produto')
+            
+            if 'nome_produto' not in request_data:
+                return {'ERRO': 'nome do produto não informado'}, 400
+            nome_produto = request_data.get('nome_produto')
+            
+            if 'descricao_produto' not in request_data: 
+                return {'ERRO': 'descrição do produto não informada'}, 400
+            descricao_produto = request_data.get('descricao_produto')
+            
+            if 'quantidade_por_unidade_produto' not in request_data:
+                return {'ERRO': 'quantidade por unidade do produto não informada'}, 400
+            quantidade_por_unidade_produto = request_data.get('quantidade_por_unidade_produto')
+            
+            if 'notificacao_baixo_estoque_produto' not in request_data:
+                return {'ERRO': 'notificação de baixo estoque do produto não informada'}, 400
+            notificacao_baixo_estoque_produto = int(request_data.get('notificacao_baixo_estoque_produto'))
+            
+            data_produto_novo = {'marca_produto': marca_produto, 'nome_produto': nome_produto, 'descricao_produto': descricao_produto, 'quantidade_por_unidade_produto': quantidade_por_unidade_produto, 'notificacao_baixo_estoque_produto': notificacao_baixo_estoque_produto}
+            
+            mongo.db.produtos.insert_one(data_produto_novo)
+            return {"SUCESSO" :'Produto Adicionado com sucesso!'}, 201
+        
+        except:
+            return {'ERRO': 'Erro ao tentar adicionar produto na base de dados'}, 500
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
