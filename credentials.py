60df1c0aae9f8142e021ae1b4637ac5596cd5798
DeenaElOrra <deenaeo@al.insper.edu.br>
2023-11-07 13:35:26 -0300

    credentials

---
+++ credentials.py
@@ -0,0 +1,9 @@
+credenciais = {
+    "user_mongo": "admin",
+    "password_mongo": "deenathecoffee"
+}
+
+settings = {
+    "host": "cluster0.qacz4xc.mongodb.net",
+    "database": "farma-proj-agil"
+}
